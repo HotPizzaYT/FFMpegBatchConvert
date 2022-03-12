3521685796f3b68a4a6c522df3bc72638dbcdfb3
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2022-03-12 14:53:40 -0600

    First commit

---
+++ FFMpegBatchConvert/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace FFMpegBatchConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.custom = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.vidscale = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog3 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.inputExt = new System.Windows.Forms.ComboBox();
+            this.outputExt = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(775, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pick FFMpeg directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FFMpeg Directory";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(775, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select input folder...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.vidscale);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.custom);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(13, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 308);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resolution settings";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(35, 87);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 101);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Custom";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // custom
+            // 
+            this.custom.AutoSize = true;
+            this.custom.Location = new System.Drawing.Point(18, 63);
+            this.custom.Name = "custom";
+            this.custom.Size = new System.Drawing.Size(61, 18);
+            this.custom.TabIndex = 4;
+            this.custom.TabStop = true;
+            this.custom.Text = "Custom";
+            this.custom.UseCompatibleTextRendering = true;
+            this.custom.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resolution";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.outputExt);
+            this.groupBox2.Controls.Add(this.inputExt);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(320, 211);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(467, 174);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bitrate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4k",
+            "8k",
+            "16k",
+            "32k",
+            "64k",
+            "128k",
+            "256k",
+            "512k",
+            "1m",
+            "8m",
+            "16m"});
+            this.comboBox1.Location = new System.Drawing.Point(77, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(70, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Bitrate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Input path will be here";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(467, 46);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Convert!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(320, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(467, 44);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Select output folder...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // vidscale
+            // 
+            this.vidscale.FormattingEnabled = true;
+            this.vidscale.Items.AddRange(new object[] {
+            "144",
+            "240",
+            "320",
+            "512",
+            "640",
+            "720",
+            "1080"});
+            this.vidscale.Location = new System.Drawing.Point(63, 204);
+            this.vidscale.Name = "vidscale";
+            this.vidscale.Size = new System.Drawing.Size(190, 21);
+            this.vidscale.TabIndex = 6;
+            this.vidscale.SelectedIndexChanged += new System.EventHandler(this.vidscale_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Scale";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(6, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "HxSoft (C) 2022-2022";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(518, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Output path";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(79, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Input extension";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 13);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Output extension";
+            // 
+            // inputExt
+            // 
+            this.inputExt.FormattingEnabled = true;
+            this.inputExt.Items.AddRange(new object[] {
+            ".mp4",
+            ".mpeg",
+            ".avi",
+            ".webm",
+            ".mkv"});
+            this.inputExt.Location = new System.Drawing.Point(135, 94);
+            this.inputExt.Name = "inputExt";
+            this.inputExt.Size = new System.Drawing.Size(56, 21);
+            this.inputExt.TabIndex = 6;
+            this.inputExt.SelectedIndexChanged += new System.EventHandler(this.inputExt_SelectedIndexChanged);
+            // 
+            // outputExt
+            // 
+            this.outputExt.FormattingEnabled = true;
+            this.outputExt.Items.AddRange(new object[] {
+            ".mp4",
+            ".mpeg",
+            ".avi",
+            ".webm",
+            ".mkv"});
+            this.outputExt.Location = new System.Drawing.Point(135, 111);
+            this.outputExt.Name = "outputExt";
+            this.outputExt.Size = new System.Drawing.Size(56, 21);
+            this.outputExt.TabIndex = 7;
+            this.outputExt.SelectedIndexChanged += new System.EventHandler(this.outputExt_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "FFMpeg Batch Convert";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton custom;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox vidscale;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox outputExt;
+        private System.Windows.Forms.ComboBox inputExt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ FFMpegBatchConvert/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FFMpegBatchConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            inputExt.Text = ".mp4";
+            outputExt.Text = ".mp4";
+            vidscale.Text = "720";
+
+
+        }
+        public string ffmpegPath = "";
+        public string outputPath = "";
+        public string inputPath = "";
+        public string inext = ".mp4";
+        public string outext = ".mp4";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowNewFolderButton = true;
+            folderBrowserDialog1.ShowDialog();
+            ffmpegPath = folderBrowserDialog1.SelectedPath;
+            label1.Text = ffmpegPath;
+            // MessageBox.Show(ffmpegPath);
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Select input folder
+            // MessageBox.Show(ffmpegPath);
+
+            folderBrowserDialog2.ShowNewFolderButton = true;
+            folderBrowserDialog2.ShowDialog();
+            inputPath = folderBrowserDialog2.SelectedPath;
+            label2.Text = folderBrowserDialog2.SelectedPath;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Convert
+            if(ffmpegPath == "" || outputPath == "" || inputPath == "" || inext == "" || outext == "") {
+
+                MessageBox.Show("You did not pick FFMpeg path, input, or output!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else if(vidscale.Text != "" && comboBox1.Text != "")
+            {
+                //check if scale and bitrate have been selected
+                MessageBox.Show(vidscale.Text);
+                MessageBox.Show(outputPath);
+
+                // string strCmdText;
+
+
+                string cmd = "for /F \"tokens=1 delims=.\" %%a in ('dir /B \"" + inputPath + "\\*" + inext + "\"') do \"" + ffmpegPath + "\\ffmpeg.exe\"  -y -i \"" + inputPath + "\\%%a" + inext + "\" -vf \"scale=" + vidscale.Text + ":-2\" -b:v " + comboBox1.Text + " -b:a " + comboBox1.Text + "  \"" + outputPath + "\\%%a" + outext + "\""; ;
+                // cmd = "echo Test!";
+                string[] batch =
+                {
+                    "@echo off", "title Converting...", cmd, "pause", "exit"
+                };
+                File.WriteAllLines("gen.bat", batch);
+                // ExecuteCommandSync("gen.bat");
+                if(MessageBox.Show("This process will replace any existing file\n\nAre you sure you want to proceed?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process.Start("cmd.exe", "/C gen.bat");
+                } else
+                {
+                    MessageBox.Show("Conversion canceled", "Canceled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                
+                string q = "";
+            } else
+            {
+                MessageBox.Show("You did not select scale and bitrate!");
+            }
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/K " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = false;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                MessageBox.Show(result);
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog3.ShowNewFolderButton = true;
+            folderBrowserDialog3.ShowDialog();
+            outputPath = folderBrowserDialog3.SelectedPath;
+            label7.Text = outputPath;
+        }
+
+        private void inputExt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            inext = inputExt.Text;
+        }
+
+        private void outputExt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            outext = outputExt.Text;
+        }
+
+        private void vidscale_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
